dca7bbfdaec28d35f8177d54ae3f26bf451ce761
Chris9294 <christophepaxion@sfr.fr>
2026-04-26 00:21:18 +0200

    Add files via upload

---
+++ src/data/patternColors.ts
@@ -0,0 +1,14 @@
+export const patternColors = [
+  "#B65CFF",
+  "#F27955",
+  "#E8EF5B",
+  "#58D846",
+  "#31C7E7",
+  "#279DDF",
+  "#8D67D9",
+  "#EC42C7",
+  "#EA7A3E",
+  "#FFB6A3",
+];
+
+export const COLORS_PER_ROW = 10;
